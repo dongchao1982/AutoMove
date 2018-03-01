1b78cf4b25bc066cd1e1ed701979b265db51127e
dongchao1982 <dong_chao1982@163.com>
2018-03-01 19:25:03 +0800

    添加镜头fov及成相截面的计算工具类

---
+++ AutoMovie/code/CameraLens.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMovie
+{
+    class CameraLens
+    {
+        //角度FOV，Angular FOV
+        public static double AFOV(double h,double f)
+        {
+            return 2 * Math.Atan(h / (2 * f)) * 180 / Math.PI;
+        }
+
+        public static double HFOV(double wd,double fov)
+        {
+            return 2 * wd * Math.Tan(fov / 2);
+        }
+    }
+}
